eb18d867d4b1dceba89cc1f3ec6219bb1010ba04
Usuario <Usuario@OLAZABAL>
2023-09-20 22:52:20 -0500

    ya esta

---
+++ pedrito/src/main/java/com/mycompany/pedrito/Pedrito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pedrito;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Pedrito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
